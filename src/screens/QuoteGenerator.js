88b58afd344217a46a3f2abe3b3dff6160953c17
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-01 10:33:49 +0100

    added a quote generator

--- src/screens/QuoteGenerator.js
+++ src/screens/QuoteGenerator.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
 const QuoteGenerator = () => {
+
+  const [quote, setQuote] = useState("")
+  const [author, setAuthor] = useState("")
+
+  const api_url = "https://zenquotes.io/api/quotes/";
+
+  async function getapi(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    let quote = data[0].q
+    let author = data[0].a
+    console.log(data);
+    setQuote(quote)
+    setAuthor(author)
+  }
+  useEffect(() => {
+    getapi(api_url)
+  }, [])
+
+
   return (
     <View>
-      <Text>QuoteGenerator</Text>
+      <Text>{quote}</Text>
+      <Text>{author}</Text>
     </View>
-  );
-};
+  )
+}
 
-export default QuoteGenerator;
+export default QuoteGenerator
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+})
+
