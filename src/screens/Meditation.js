629e538d18f60698a344ed502488762b64a7ee85
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-08-02 11:16:00 +0100

    Timer added, struggling with functionality of capturing value from slider and feeding to timer

--- src/screens/Meditation.js
+++ src/screens/Meditation.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import ModalMed from "../Components/ModalMeditation";
 import Timer from "../Components/Timer";
+import beach from "../../assets/beach.jpg"
 
 // our meditation timer start function
 function consolelog() {
@@ -9,7 +10,7 @@ function consolelog() {
 }
 
 const Meditation = () => {
-  const [TimerLength, setTimerLength] = useState(30);
+  const [TimerLength, setTimerLength] = useState(300);
   const TimerLengthChange = (sliderValue) => {
     // const value = e.target.value;
     setTimerLength(sliderValue);
@@ -17,6 +18,7 @@ const Meditation = () => {
   return (
     <View>
       <Text>Meditation</Text>
+      {/* <Image styles={{backgroundImage: `url(${beach})`}}/> */}
       <ModalMed changeState={TimerLengthChange} innerText={"Please select times"}></ModalMed>
       <Timer TimerLength={TimerLength}/>
     </View>
@@ -25,4 +27,5 @@ const Meditation = () => {
 
 export default Meditation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+});
