9795aa6499ecfcdaa338bf11831ec87bd4fa4889
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-08-02 15:16:10 +0100

    reduce clickable area to bird house

--- src/Components/modal.js
+++ src/Components/modal.js
@@ -130,6 +130,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 22,
   },
+  // modalViewContainer: {
+  //   width:
+  // },
   modalView: {
     margin: 20,
     backgroundColor: "white",
@@ -157,7 +160,7 @@ const styles = StyleSheet.create({
   },
 
   petHouseWrapper: {
-    width: "100%",
+    width: "fitContent",
     alignItems: "center",
   },
   petHouse: {
