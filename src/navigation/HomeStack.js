21f5aad92466257c69dfc2c486374b825dca8a80
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-04 11:00:55 +0100

    made eggs on egg screen pressable, next step change egg to hatching gif when clicked

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -5,6 +5,7 @@ import Meditation from "../screens/Meditation";
 import MoodLogger from "../screens/MoodLogger";
 import QuoteGenerator from "../screens/QuoteGenerator";
 import Stats from "../screens/stats";
+import Eggs from "../screens/Eggs"
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export default function HomeStack() {
   return (
     // Once all navigation functionality added into each page, we can screenOptions={{ headerShown: false }} after <Stack.Navigator>
     <Stack.Navigator screenOptions={{ headerShown: true }}>
+      <Stack.Screen name="Eggs" component={Eggs} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Meditation" component={Meditation} />
       <Stack.Screen name="MoodLogger" component={MoodLogger} />
