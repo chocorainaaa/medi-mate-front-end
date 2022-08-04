fafb130e3a2e46410585b53d9a75b7fca3e0312d
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-04 11:00:55 +0100

    made eggs on egg screen pressable, next step change egg to hatching gif when clicked

---
+++ src/screens/Eggs.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View, Image, Button, useWindowDimensions, Pressable } from 'react-native'
+import React from 'react'
+
+export default function Eggs() {
+
+    // use this for media query type shannanigans
+    const { height, width } = useWindowDimensions()
+
+    function handleClick() {
+        console.log("im not ready to hatch")
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.hatchery}>
+                <Pressable style={{
+                    height: 200,
+                    width: 100,
+                }}
+                    onPress={handleClick}>
+                    <Image style={styles.egg} source={require("../../assets/Egg/Egg.png")} />
+                </Pressable>
+                <Pressable style={{
+                    height: 200,
+                    width: 100,
+                }}
+                    onPress={handleClick}>
+                    <Image style={styles.egg} source={require("../../assets/Egg/Egg.png")} />
+                </Pressable>
+                <Pressable style={{
+                    height: 200,
+                    width: 100,
+                }}
+                    onPress={handleClick}>
+                    <Image style={styles.egg} source={require("../../assets/Egg/Egg.png")} />
+                </Pressable>
+            </View>
+            <Text>Please choose your Medi-Mate</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        width: '90%',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: 'solid',
+        borderwidth: 20,
+    },
+    hatchery: {
+        alignSelf: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    egg: {
+        height: 200,
+        width: 100,
+        resizeMode: 'contain'
+    },
+    // egg2: {
+    //     backgroundImage: url("../../assets/Egg/Egg.png")
+    // },
+    // egg2: {
+    //     backgroundImage: url("../../assets/Egg/Egg.png")
+    // },
+})
