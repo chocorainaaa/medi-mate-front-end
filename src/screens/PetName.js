3ab4443260d4d9ca7e650f2bd052607afb858268
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-04 11:47:51 +0100

    positioning is better, working on a button for setting the name

--- src/screens/PetName.js
+++ src/screens/PetName.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Image, View, TextInput, Text, Pressable } from 'react-native'
 import React, { useState } from 'react'
 
 
@@ -6,23 +6,42 @@ export default function PetName() {
 
     const [petName, setPetName] = useState("");
 
+    function nameChoosen() {
+        console.log(petName)
+    }
+
     return (
         <View style={styles.container}>
+
+            <Image style={styles.bird} source={require("../../assets/Bird/bird.gif")} />
+
+
             <TextInput
-                placeholder="Please choose a name for your Medi-Mate"
+                placeholder="Name"
                 value={petName}
                 onChangeText={(text) => setPetName(text)}
                 style={styles.input}
             />
+            <Pressable onPress={nameChoosen} />
+            <Text style={styles.text}>Please choose a name for your Medi-Mate</Text>
         </View>
     )
+
 }
 
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
-        alignself: 'center',
-        justifyContent: 'center'
+        width: '90%',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: 'solid',
+        borderwidth: 20,
+    },
+    bird: {
+        height: 400,
+        width: 400
     },
     input: {
         backgroundColor: "white",
@@ -30,5 +49,8 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         borderRadius: 10,
         marginTop: 5,
+    },
+    text: {
+        marginTop: 25,
     }
 })
