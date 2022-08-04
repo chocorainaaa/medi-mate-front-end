9785c61cdf21c566e382d6bbff612954f138fd31
chocorainaaa <blackettraina@gmail.com>
2022-08-04 13:21:53 +0100

    Stats page now has text boxes with relevant text in them. Placed on the page. Background image has some minor issues to debug at a later date. No functionality yet as this page will interact with our DB through the back-end

--- src/screens/stats.js
+++ src/screens/stats.js
@@ -1,14 +1,157 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  useWindowDimensions,
+} from "react-native";
 import React from "react";
+import { ImageBackground } from "react-native-web";
 
 const Stats = () => {
+  const { height, width } = useWindowDimensions();
+
+  function petImageSize() {
+    return Math.floor(width / 1);
+  }
+
+  const images = {
+    background: require("../../assets/background/forest-background_200_640x640.png"),
+    pet: require("../../assets/Bird/bird-1.png.png"),
+    textBox: require("../../assets/text-boxes/Text-box.png"),
+  };
+
   return (
-    <View>
-      <Text>Stats</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        style={{
+          width: "100%",
+          height: "105%",
+          position: "absolute",
+          resizeMode: "center",
+          resizeMethod: "center",
+        }}
+        source={images.background}
+      >
+        {/* <View>
+          <Image
+            resizeMode="contain"
+            style={{
+              width: petImageSize(),
+              height: petImageSize(),
+              top: height / 2.3, //778 / 300 = 2.59
+              marginLeft: width / 30,
+              position: "relative",
+            }}
+            source={images.pet}
+          />
+        </View> */}
+
+        <View style={styles.container}>
+          <View style={styles.textView}>
+            <ImageBackground
+              source={images.textBox}
+              style={styles.coverImage}
+            />
+
+            <Text style={styles.imageText}>Mood log</Text>
+            <ImageBackground />
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textView}>
+            <ImageBackground
+              source={images.textBox}
+              style={styles.coverImage}
+            />
+
+            <Text style={styles.imageText}>Number of visits:</Text>
+            <ImageBackground />
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textView}>
+            <ImageBackground
+              source={images.textBox}
+              style={styles.coverImage}
+            />
+
+            <Text style={styles.imageText}>Total meditation time:</Text>
+            <ImageBackground />
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textView}>
+            <ImageBackground
+              source={images.textBox}
+              style={styles.coverImage}
+            />
+
+            <Text style={styles.imageText}>Daily streak:</Text>
+            <ImageBackground />
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textView}>
+            <ImageBackground
+              source={images.textBox}
+              style={styles.coverImage}
+            />
+
+            <Text style={styles.imageText}>Pet age:</Text>
+            <ImageBackground />
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textView}>
+            <ImageBackground
+              source={images.textBox}
+              style={styles.coverImage}
+            />
+            <Text style={styles.imageText}>Reward points:</Text>
+            <ImageBackground />
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
-export default Stats;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  coverImage: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+  },
+  imageText: {
+    fontSize: 20,
+    color: "black",
+    fontWeight: "bold",
+    position: "absolute",
+  },
+  textView: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  textBox: {
+    width: 17,
+    height: 17,
+    position: "relative",
+    justifyContent: "center",
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Stats;
