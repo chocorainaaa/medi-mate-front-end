eff8be1ab50923a2bb3806104e18983bd6cd888c
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-05 13:43:22 +0100

    sliders capturing value, timer starts on click meditate, next steps are starting animation on click meditate

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,7 +1,7 @@
 import { StyleSheet, View, Text, TextInput } from "react-native";
 import React from "react";
-
-const UniSlider = ({ min, max, id, onChange, lengthChange, breathChange, holdChange, sliderValue, label }) => {
+import Slider from '@react-native-community/slider'
+const UniSlider = ({ min, max, id, onChange, lengthChange, breathChange, holdChange, lengthValue, setLengthValue, label, sliderValue }) => {
 
   // useEffect((sliderValue) => {
   //   setTimer(sliderValue)
@@ -11,16 +11,18 @@ const UniSlider = ({ min, max, id, onChange, lengthChange, breathChange, holdCha
   // change the Textinput to a range slider 
 
   return (
+
     <View style={styles.parent}>
-      <TextInput
+      <Slider
         style={styles.slider}
-        type="range"
-        min={min}
-        max={max}
+        minimumValue={min}
+        maximumValue={max}
         value={sliderValue}
-        className="slider"
+        step={1}
         id={id}
-        onChange={onChange}
+        onValueChange={
+          (sliderValue) => onChange(sliderValue)
+        }
       />
       <Text>{label}</Text>
     </View>
@@ -34,7 +36,6 @@ const styles = StyleSheet.create({
     width: 250,
   },
   slider: {
-    backgroundColor: "blue",
     color: "white",
   },
 });
