20585a9750979d60f26f63ebb209d53c2d77eddc
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-08-08 15:33:19 +0100

    moved meditation rings to be centred on pet & fixed timer navigation issue when 0

--- src/screens/Meditation.js
+++ src/screens/Meditation.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ModalMed from "../Components/ModalMeditation";
 import Timer from "../Components/Timer";
 import AnimatedRing from "../Components/Ring";
@@ -11,6 +11,7 @@ const Meditation = () => {
   const [breathValue, setBreathValue] = useState(5);
   const [holdValue, setHoldValue] = useState(5);
   const [sliderValue, setSliderValue] = useState(15);
+  const [timer, setTimer] = useState(0)
 
   function lengthChange(sliderValue) {
     setLengthValue(sliderValue);
@@ -21,6 +22,12 @@ const Meditation = () => {
   function holdChange(sliderValue) {
     setHoldValue(sliderValue);
   }
+
+  useEffect(() => {
+    if (startMeditation && timer <= 0) {
+      navigation.navigate("Home")
+    }
+  }, [timer])
 
   return (
     <ImageBackground
@@ -39,7 +46,7 @@ const Meditation = () => {
           lengthValue={lengthValue}
           innerText={"Please select times"}
         ></ModalMed>
-        <Timer lengthValue={lengthValue} startMeditation={startMeditation} />
+        <Timer timer={timer} setTimer={setTimer} lengthValue={lengthValue} startMeditation={startMeditation} />
       </View>
 
       <View style={styles.birdCage}>
@@ -65,9 +72,13 @@ const styles = StyleSheet.create({
   bird: {
     height: 200,
     width: 200,
+    position: "absolute",
   },
   birdCage: {
-    paddingTop: 400,
-    paddingLeft: 150
+    paddingTop: 500,
+    paddingLeft: 150,
+    flex: "display",
+    justifyContent: "center",
+    alignItems: "center",
   }
 });
