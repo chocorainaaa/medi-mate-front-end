02f6daa3886caacdbab1903567be30b1524cc319
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-11 10:29:12 +0100

    enviroment variables now in a .env file

--- config/firebase.js
+++ config/firebase.js
@@ -1,33 +1,20 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase";
 import "firebase/auth";
-import Constants from "expo-constants";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  // apiKey: Constants.manifest.extra.apiKey,
-  // authDomain: Constants.manifest.extra.authDomain,
-  // projectId: Constants.manifest.extra.projectId,
-  // storageBucket: Constants.manifest.extra.storageBucket,
-  // messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  // appId: Constants.manifest.extra.appId,
-
-  apiKey: "AIzaSyDzrWiH_wehV_4QQnqgXHlHQ_gHaKV9U3c",
-  authDomain: "medimate-91beb.firebaseapp.com",
-  projectId: "medimate-91beb",
-  storageBucket: "medimate-91beb.appspot.com",
-  messagingSenderId: "662305892755",
-  appId: "1:662305892755:web:6183ec76c413ea8c09e066",
-
-  //   API_KEY=AIzaSyDzrWiH_wehV_4QQnqgXHlHQ_gHaKV9U3c
-  // AUTH_DOMAIN=medimate-91beb.firebaseapp.com
-  // PROJECT_ID=medimate-91beb
-  // STORAGE_BUCKET=medimate-91beb.appspot.com
-  // MESSAGING_SENDER_ID=662305892755
-  // APP_ID=1:662305892755:web:6183ec76c413ea8c09e066
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
