bc4c75406b736809db55c0aa340a41b89fafd82d
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-08-11 11:06:40 +0100

    Login & Registration buttons styled to perfection

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -135,7 +135,8 @@ export default function LoginScreen() {
           </TouchableOpacity>
         </View>
 
-        <Image resizeMode="contain" style={styles.egg} source={images.egg} />
+        <Image style={styles.egg} source={images.egg} />
+      
         <View style={styles.contact}>
           <Button
             onPress={() => Linking.openURL("mailto:support@example.com")}
@@ -190,23 +191,22 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   buttonContainer: {
-    width: "60%",
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 40,
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
   },
   button: {
     backgroundColor: "#0782F9",
-    width: "100%",
+    width: 80,
     padding: 15,
     borderRadius: 10,
     alignItems: "center",
   },
   buttonOutline: {
     backgroundColor: "white",
-    marginTop: 5,
-    borderColor: "#0782F9",
-    borderWidth: 2,
   },
   buttonText: {
     color: "white",
