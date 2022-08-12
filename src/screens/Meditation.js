7198c0a543ce656ba10362d1d049254d3cece9c9
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-12 12:05:48 +0100

    rings are now centered on the bird, in web, on android, ios needs checking

--- src/screens/Meditation.js
+++ src/screens/Meditation.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 import ModalMed from "../Components/ModalMeditation";
 import Timer from "../Components/Timer";
 import AnimatedRing from "../Components/Ring";
-
+import {
+  horizontalScale,
+  verticalScale,
+  moderateScale,
+} from "../Components/Metrics";
 const Meditation = ({ navigation }) => {
   const [startMeditation, setStartMeditation] = useState(false);
   const [lengthValue, setLengthValue] = useState(5);
@@ -77,8 +81,12 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   birdCage: {
-    paddingTop: 500,
-    paddingLeft: 150,
+    borderColor: "black",
+    borderWidth: 10,
+    height: verticalScale(500),
+    width: horizontalScale(400),
+    marginRight: horizontalScale(200),
+    marginTop: verticalScale(25),
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
