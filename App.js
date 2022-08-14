d6317d02bf0495eb9642bbd8c5cfc168ddf141cb
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-14 16:03:17 +0100

    in firebase.js I changed the way we are importing firebase

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Routes from "./src/navigation";
 import "react-native-gesture-handler";
 import AppLoading from "expo-app-loading";
-import { useFonts, VT323_400Regular } from "@expo-google-fonts/vt323";
+// import { useFonts, VT323_400Regular } from "@expo-google-fonts/vt323";
 
 import { StyleSheet, Text, View } from "react-native";
 
