c162a03ebe79f544f4794e2eae33501de5eb31e0
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-14 16:52:00 +0100

    uncommented AppLoading

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React from "react";
 import Routes from "./src/navigation";
 import "react-native-gesture-handler";
 import AppLoading from "expo-app-loading";
-// import { useFonts, VT323_400Regular } from "@expo-google-fonts/vt323";
+import { useFonts, VT323_400Regular } from "@expo-google-fonts/vt323";
 
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  // let [fontsLoaded] = useFonts({
-  //   VT323_400Regular,
-  // });
+  let [fontsLoaded] = useFonts({
+    VT323_400Regular,
+  });
 
-  // if (!fontsLoaded) {
-  //   return <AppLoading />;
-  // }
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return <Routes />;
 }
