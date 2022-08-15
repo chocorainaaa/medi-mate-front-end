17b26e613d821f1995e59d9b82869a83161fd145
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-15 09:11:03 +0100

    changed the firebase import path

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 // import * as firebase from 'firebase/app';
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
